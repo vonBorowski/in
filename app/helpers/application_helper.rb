9cca54af143c58ed665eaa86f53eaff1888b620f
Guilherme Taschetto <gtaschetto@gmail.com>
2014-12-12 17:52:19 -0200

    Add helper to create collapsible ibox float div with title.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,10 +15,30 @@ module ApplicationHelper
   end
 
   def actions(resource)
-    output = ''
-    output << link_to(raw("#{fa_icon "pencil"}"), [:edit, resource], class: "btn btn-primary btn-outline", title: "Editar")
-    output << link_to(raw("#{fa_icon "trash"}"), resource, method: :delete, class: "btn btn-danger btn-outline", data: {confirm: "Tem certeza?"}, title: "Remover")
-    output = content_tag(:div, output.html_safe, class: "btn-group")
-    output.html_safe
+    content_tag(:div, class: "btn-group") do
+      concat link_to(raw("#{fa_icon "pencil"}"), [:edit, resource], class: "btn btn-primary btn-outline", title: "Editar")
+      concat link_to(raw("#{fa_icon "trash"}"), resource, method: :delete, class: "btn btn-danger btn-outline", data: {confirm: "Tem certeza?"}, title: "Remover")
+    end
+  end
+
+  def ibox(params)
+    ibox_class = "ibox float-e-margins"
+    ibox_class << " collapsed border-bottom" if params[:collapsed] == true
+    content_tag(:div, class: ibox_class) do
+      content_tag(:div, class: "ibox-title") do
+        content_tag(:h5, params[:title]) +
+        content_tag(:div, class: "ibox-tools") do
+          if params[:collapsed].blank?
+            content_tag(:a, raw("#{fa_icon "chevron-up"}"), class: "collapse-link")
+          else
+            content_tag(:a, raw("#{fa_icon "chevron-up"}"), class: "collapse-link") if params[:collapsed] == false
+            content_tag(:a, raw("#{fa_icon "chevron-down"}"), class: "collapse-link") if params[:collapsed] == true
+          end
+        end
+      end +
+      content_tag(:div, class: "ibox-content") do
+        yield
+      end
+    end
   end
 end
